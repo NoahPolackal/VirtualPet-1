2e846ec8c7529cf203d2222d949c7b28dad243f5
Noah Polackal <71651076+NoahPolackal@users.noreply.github.com>
2020-12-01 14:41:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+
+var dog,happydDog,database,foodStock,foodS;
+
+
+
+
+function preload(){
+  //load images here
+  dogimg = loadImage("images/dogImg.png");
+  dogimg2 = loadImage("images/dogImg1.png");
+}
+
+function setup() {
+  var canvas = createCanvas(500, 500);
+  database = firebase.database();
+  foodStock = database.ref('food');
+  //foodStock.on("value",readStock);
+  dog = createSprite(250,250,10,10);
+  dog.addImage(dogimg);
+  dog.scale = 0.2;
+  }
+ function draw() {  
+  background("gold");
+      if(keyWentDown(UP_ARROW)){
+      //writeStock(foodS);
+      dog.addImage(dogimg2);
+ }
+
+  drawSprites();
+  //add styles here
+  fill("black");
+  textSize(20);
+  stroke(10);
+  text("Press Up Arrow Key To Feed Mike Milk :)",80,100);
+  
+}
+function readStock(data){
+  foodS=data.val();
+}
+function writeStock(x){
+database.ref('/').update({
+  food:x
+})
+}
